ba7f980a4fdfe2a8a00de5d40973f2282bf7fcb9
JeremyPeng-NB <JeremyPeng@yuchens.com>
2021-01-25 16:23:48 +0800

    檔案全部拉上去拉
    檔案全部拉上去拉 哈哈哈哈 by Jeremy

---
+++ MXIC_PCCS/DataUnity/Interface/ISwipeInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MXIC_PCCS.DataUnity.Interface
+{
+    interface ISwipeInfo
+    {
+        string CheckinList(string VendorName, string EmpID, string EmpName ,DateTime? StartTime, DateTime? EndTime, string AttendTypeSelect);
+
+        string SwipeInfoDetail(string EditID);
+
+        string EditSwipe(string EditID, string AttendTypeSelect, string Hour);
+
+         string transform();
+        void transform2(string StartTime, string EndTime,string PoNo);
+    }
+}
